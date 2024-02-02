f3b10fda423d595f430e5d96e18b216d5e62f7a3
joe <maloney.jg2@gmail.com>
2024-02-01 23:03:06 -0500

    kfjdsklfjlskajf

--- web/src/app/navbar/navbar.component.ts
+++ web/src/app/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { ConnectionStateService } from '../connection-state.service';
 import { Observable } from 'rxjs';
+import { SvdParserService } from '../svd-parser.service';
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -14,18 +15,19 @@ import { Observable } from 'rxjs';
 export class NavbarComponent {
   settingsVisible: boolean;
   ConnectionStateService: ConnectionStateService;
+  SvdParserService: SvdParserService;
   selected = "option2";
   allowedPollingRates: Array<number>;
   selectedPollingRate: Number;
   connectionStatus: String = "test_string";
   private connectionStatus$: Observable<string>;
-  constructor(ConnectionStateService: ConnectionStateService) {
+  constructor(ConnectionStateService: ConnectionStateService, SvdParserService: SvdParserService) {
     this.settingsVisible = false;
     this.allowedPollingRates = [1, 2, 3, 5, 10, 30, 60, 90, 120, 180, 240, 300, 360];
     this.selectedPollingRate = 5;
     this.ConnectionStateService = ConnectionStateService;
     this.connectionStatus$ = this.ConnectionStateService.connectionStateObservable();
-
+    this.SvdParserService = SvdParserService;
 
     this.connectionStatus$.subscribe(state => {
       this.connectionStatus = state;
@@ -35,6 +37,7 @@ export class NavbarComponent {
   }
   settingsClickHandler() {
     this.settingsVisible = !this.settingsVisible;
+
   }
   pollingRateClickHandler(rate: Number) {
     this.selectedPollingRate = rate;
--- web/src/app/svd-parser.service.ts
+++ web/src/app/svd-parser.service.ts
@@ -1,14 +1,40 @@
 import { Injectable } from '@angular/core';
-
+import xml2js from 'xml2js';
 @Injectable({
   providedIn: 'root'
 })
 export class SvdParserService {
 
-  constructor() { }
-  parseSvd() {
+  private mainJson: Object;
+  constructor() {
+    this.mainJson = {};
 
   }
+  public getJson() {
+    if (this.mainJson) {
+      return this.mainJson;
+    }
+    else {
+      console.error("mainJson retrival attempted without a loaded svg file");
+      return "mainJson not loaded";
+    }
+  }
+  public parseSvd(svdString: string) {
+    xml2js.parseString(svdString, (err, result) => this.svdParseCallback(err, result));
+
+
+  }
+  private svdParseCallback(err: Error | null, result: any) {
+    if (err) {
+      console.error(err);
+    }
+    else {
+      this.mainJson = result;
+      console.log(`json loaded\n ${this.mainJson}`);
+    }
+  }
+
+
   getPeriphrials() {
 
   }
